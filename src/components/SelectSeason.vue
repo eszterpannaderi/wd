f3b2bf576f0c4e0f93b71ba22fd829ef3daf1407
Your Name <you@example.com>
2022-06-18 12:47:51 +0200

    wd

---
+++ src/components/SelectSeason.vue
@@ -0,0 +1,26 @@
+<template>
+  <label for="season" class="col-sm-1 col-form-label">Season</label>
+  <div class="col-sm-2">
+<select  
+  @change="$emit('onSelectedSeason', $event)"
+  id="season">
+      <option
+      v-for="(s,i) in seasons"
+      :key="i"
+      :value="s"
+      >{{s}}
+      </option>
+  </select></div>
+</template>
+
+<script>
+export default {
+    name:'select-season',
+    props:['seasons']
+
+}
+</script>
+
+<style>
+
+</style>
