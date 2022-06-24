856783ce20cec0e294233afb105c0c34b48fb78d
Your Name <you@example.com>
2022-06-24 16:00:35 +0200

    not working

--- src/views/WalkingDead.vue
+++ src/views/WalkingDead.vue
@@ -3,7 +3,7 @@
       <div class="row mb-5"><select-season :seasons="seasons"></select-season></div>
       <div class="row"></div>
       <episode-card
-      :seasonList="seasonList"></episode-card>
+      :seasonList="seasonList" :seasonNumber="seasonNumber"></episode-card>
   </div>
 
 </template>
@@ -12,13 +12,15 @@
 import DataService from '@/services/dataservice'
 import EpisodeCard from '@/components/EpisodeCard.vue'
 import SelectSeason from '@/components/SelectSeason.vue'
+import dataservice from '@/services/dataservice'
 export default {
     name:'walking-dead',
     components:{EpisodeCard, SelectSeason},
     data(){
         return{
             seasonList:[],
-            seasons:[]
+            seasons:[],
+            seasonNumber:1
         }
     },
     created(){
@@ -26,9 +28,30 @@ export default {
         .then(result=>{
             this.seasons=[...new Set(result.map(x => x.season))]
             console.log(this.seasons)
-            this.seasonList=result
+            //this.seasonList=result
+            dataservice.getEpisodesBySeason(this.seasons[9])
+            .then(result=>{
+                this.seasonList=result});
             console.log(this.seasonList)
         })
+    },
+    methods:{
+        onSelectedSeason(event){
+            this.seasonNumber=event.target.value;
+            dataservice.getEpisodesBySeason(event.target.value)
+            .then(result=>{
+                //this.seasonList=result
+                this.seasonList=result.map(x =>{
+                    if(x.image == null){
+                        x.image = {
+                            medium:'https://via.placeholder.com/170x120'
+                        }
+                    }
+                    return x
+                  })
+                })
+
+        }
     }
 
 }
